bf83d5050fe030b73977aba6de2470dbd6637097
Heloisa de Castro Araujo <hyugahelo@gmail.com>
2021-03-20 12:32:43 -0300

    Criação do Index

---
+++ 001PHP/resultado.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+<title>Resultado</title>
+</head>
+<body>
+<!--Cabeçalho-->
+<header>
+	<!--Lista nao ordenada-->
+	<ul>
+	<!--linhas-->
+		<li>
+		<!--SRC caminhos de arquivos-->
+		<!--width largura-->
+		<!--height altura-->
+			<img src="trigue.jpg" width="150px" height="100px" /> 
+		</li>
+		<li>
+		<!--tag 'a' link-->
+		<!--href destino do link-->
+			<a href="index.html">Home</a>
+		</li>
+		<li>
+			<a href="resultado.php">Resultado</a>
+		</li>
+	</ul><!--Fim da Lista menu-->
+
+</header> 
+<!--conteudo principal-->
+<main>
+	<h1>Resultado</h1> <!--Usado para titulos-->
+</main> 
+<!--Rodapé-->
+<footer>
+<p> heloisa----2021</p>
+</footer> 
+</body>
+</html>
